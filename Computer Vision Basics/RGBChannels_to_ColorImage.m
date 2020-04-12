19361a078fb030cd6541f985da080c187c01c513
Sumit Chakraborty <40000937+chakraborty9569@users.noreply.github.com>
2020-04-12 13:28:51 +0530

    Update RGBChannels_to_ColorImage.m

--- Computer Vision Basics/RGBChannels_to_ColorImage.m
+++ Computer Vision Basics/RGBChannels_to_ColorImage.m
@@ -1,10 +1,17 @@
+%reading image
 img=imread('image.jpg');
 [r,c]=size(img);
  
+%Extracting RGB Channels
 Blue=img(1:341,:);
 Green=img(342:682,:);
 Red=img(683:1023,:);
+
+%combining RGB Channels
 ColImg=cat(3,Blue,Green,Red);
 
+%displaying image
 imshow(ColImg)
+ 
+%saving image
 imwrite(ColImg,'ColourImage.jpg');
